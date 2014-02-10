b0d8ac7761ae6b76aa804e2d0b81359f88ed9297
3rd-Eden <info@3rd-Eden.com>
2014-02-10 09:55:12 +0100

    [fix] Fixed a property typo

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ FullContact.prototype.request = function req(packet, args) {
   request(packet, function requested(err, res, body) {
     if (err) return fn(err);
 
-    self.ratereset = +res.headers['x-rate-limit-reset'] || self.ratereste;
+    self.ratereset = +res.headers['x-rate-limit-reset'] || self.ratereset;
     self.ratelimit = +res.headers['x-rate-limit-limit'] || self.ratelimit;
     self.remaining = +res.headers['x-rate-limit-remaining'] || self.remaining;
 
