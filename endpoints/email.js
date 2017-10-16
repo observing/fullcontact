57ecbf115bef5e51f3edd4d82aa632c2d94f9a1b
Stephen Lombard <slombard35@gmail.com>
2017-10-16 21:17:29 +0800

    Replaced email endpoint with something that works per latest specs

--- endpoints/email.js
+++ endpoints/email.js
@@ -10,27 +10,27 @@
 function Email(api) {
   this.api = api;
 
-  this.endpoint = 'https://api.fullcontact.com/' + api.version + '/email/';
+  this.endpoint = 'https://api.fullcontact.com/' + api.version + '/verification/';
   this.send = api.process.bind(api, this);
 }
 
 /**
- * Check if we we're given a disposable e-mail address.
+ * Verifies the existance of an email address.
  *
  * ```js
- * fullcontact.email.disposable('foo@bar.bar', fn);
+ * fullcontact.email.verify('foo@bar.bar', fn);
  * ```
  *
  * @returns {Email}
  * @api public
  */
-Email.prototype.disposable = function disposable() {
+Email.prototype.verify = function verify() {
   var args = this.api.args(arguments);
 
   //
   // Add a custom endpoint.
   //
-  args.endpoint = this.endpoint + 'disposable.json';
+  args.endpoint = this.endpoint + 'email';
 
   this.send({ email: args.value }, args);
   return this;
